c5fd07edad3c86fa45cad391a439ee59300bfce3
HARSHIT KUMAR SISODIA <118814588+harrsh-it@users.noreply.github.com>
2023-12-11 18:03:50 +0000

    datatypes done

---
+++ 01_basics/03_datatype.js
@@ -0,0 +1,29 @@
+"use strict";                                  //treat all js code as newer version
+
+// alert("hello")
+// alert(3 + 3)                               // we are using node js not browser
+
+console.log(3+3) ; console.log("Harshit")    // not a good way
+
+
+let name="harshit"        //string
+let age = 18             //number
+let isLoggedIn = false  //bool
+
+
+//     ***Preemetive data types***
+// number    => 2 to power53
+// bigint    => for large number
+// string    => " "
+// boolean   => true/false
+// null      => standalone value
+// Undefined => not defined yet
+// Symbol    => for uniqueness
+
+//     ***Object***
+
+console.log(typeof "harshit") 
+console.log(typeof "age")
+
+console.log(typeof null)        //object as output
+console.log(typeof undefined)  //undefined as output

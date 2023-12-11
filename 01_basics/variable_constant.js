fd584cba68d8a382bf38597ce86df71d6250e1bf
HARSHIT KUMAR SISODIA <118814588+harrsh-it@users.noreply.github.com>
2023-12-11 17:30:41 +0000

    variables difference done

---
+++ 01_basics/variable_constant.js
@@ -0,0 +1,17 @@
+const accountId = 144553               //can't change value
+let accountEmail = "harshitdharmveer"  //can change value
+var accountPassword= "1234"            //can change value(block scope & function scope problem arise)
+accountCity = "Jaipur"                 //can change value(not a good method)
+
+// accountId = 2 // not allowed
+console.log(accountId);    //for print output
+
+console.table([accountId,accountEmail,accountPassword,accountCity])   // for print multiple output
+
+
+let accountState;   //give undefined
+console.log(accountState);    //output = undefine
+
+
+
+

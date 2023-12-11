c9bf6d9dae6cc96987ffcb6c49df6f36b5083529
HARSHIT KUMAR SISODIA <118814588+harrsh-it@users.noreply.github.com>
2023-12-11 19:54:15 +0000

    Operations done

--- 01_basics/04_conversion_operation.js
+++ 01_basics/04_conversion_operation.js
@@ -94,3 +94,52 @@ let stringNumber = String(someNumber)
 console.log(stringNumber);             //33
 console.log(typeof stringNumber);     //string
 
+
+
+
+//********************************************** Operation ********************************
+console.log("*********************************** Operation ********************************")
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+
+
+let str1="Hello"
+let str2="Harsh"
+let str3=str1+str2
+console.log(str3);
+
+
+console.log("1"+2);         //12
+console.log(1+"2");        //12
+console.log("1" + "2");    //12
+
+console.log("1" +2 +2) ;     //122
+console.log(1 +2 + "2")     //32
+
+console.log(3*4*5%3);    //0
+
+console.log(+true);          //1
+// console.log(true+);      //Error
+
+console.log(+"")     //0
+
+
+let num1, num2, num3
+num1 = num2 = num3 = 3
+
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter)
+
+
